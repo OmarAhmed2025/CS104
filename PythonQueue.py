d3a7c47600172c96b3637b820e4f95a3cba5247b
OmarAhmed2025 <91150797+OmarAhmed2025@users.noreply.github.com>
2021-11-18 14:34:58 -0500

    PythonQueue

---
+++ PythonQueue.py
@@ -0,0 +1,18 @@
+#Initate Queue
+queue=[]
+#For Loop for Appening names to list
+for i in range(10):
+    Name=input("Name")
+    queue.append(Name)
+#For Loop for Popping names from list and printing
+for i in range (len(queue)):
+   print(queue.pop(0))
+
+
+
+
+
+
+
+
+ 
